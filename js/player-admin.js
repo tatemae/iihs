5d1e6f697e12b998ac42872885e58805b037c883
James Carbine <JamesCarbine@gmail.com>
2013-08-31 15:20:40 -0600

    each time the new video is added, the html code is updated on the screen

--- js/player-admin.js
+++ js/player-admin.js
@@ -32,11 +32,29 @@ $(document).ready(function() {
       video_element.find('#'+key).attr("value", value).val(value);
     });
     $('.video_elements').prepend(video_element.html());
-    var content = htmlEscape(video_element.html());
+    var content = video_html();
     var template_a = $('.page_html_template_a').html();
     var template_b = $('.page_html_template_b').html();
     var page_html = template_a + content + template_b;
     $('.page_html').html(page_html);
+  };
+
+  var video_html = function() {
+    $('#temp_container').html('');
+    $('.video_elements .video_element').each(function(index, video_element) {
+      var video_div = document.createElement('div');
+      var data = {};
+
+      $(video_element).find('input[type=text]').each(function() {
+        data[$(this).attr('name')] = $(this).val();
+      });
+      $.each(data, function(key, value) {
+        $(video_div).attr(key, value);
+      });
+      $('#temp_container').append(video_div);
+      $('#temp_container').append("\n      ");
+    });
+    return htmlEscape($('#temp_container').html());
   };
 
   var htmlEscape = function(str) {
