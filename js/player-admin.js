bf8928a4073bf0f31b10d733df9b725b437a8490
James Carbine <JamesCarbine@gmail.com>
2013-08-31 07:47:43 -0600

    added templates for admin form. filling out the form will place a copy on the page. user can edit that copy also

--- js/player-admin.js
+++ js/player-admin.js
@@ -1,11 +1,44 @@
 $(document).ready(function() {
   $('#new_video_form').submit(function(e) {
     e.preventDefault();
-    var data_id = $(this).find('[name=data-id]').val();
+    $('.edit_video').off();
+
+    add_to_page(this);
+
     $('#admin_form').modal('hide');
-    $(this).closest('form').find("input[type=text], textarea").val("");
+
+    $('.edit_video').on('click', function() {
+      var video_element = $(this).closest('.video_element');
+      var data = {};
+
+      $(video_element).find('input[type=text]').each(function() {
+        data[$(this).attr('name')] = $(this).val();
+      });
+      $.each(data, function(key, value) {
+        // var data_id = asdf.find('#data-id').attr("value");
+        // $('#admin_form form').find('#data-id').attr("value", data_id).val(data_id);
+        $('#admin_form form').find('#'+key).attr("value", value).val(value);
+      });
+    });
   });
-  // $('#add_video').click(function(e){
-  //   var data_id = $('#data-id').val();
-  // });
+
+  var add_to_page = function(elem) {
+    var data = {};
+
+    $(elem).find('input[type=text]').each(function() {
+      data[$(this).attr('name')] = $(this).val();
+    });
+
+    var video_element = $('.video_element_template').clone();
+    $.each(data, function(key, value) {
+      video_element.find('#'+key).attr("value", value).val(value);
+    });
+    $('.video_elements').prepend(video_element.html());
+  };
+
+  //
+  // Clear the modal form when it is hidden
+  $('#admin_form').on('hidden', function() {
+    $(this).find('form').find("input[type=text], textarea").val("");
+  });
 });
